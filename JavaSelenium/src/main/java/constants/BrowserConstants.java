ed9b4300026c5ddec10c5c9bdc03cca6c6e12e2a
villanuevalys <83683692+villanuevalys@users.noreply.github.com>
2021-05-06 01:20:20 +0800

    Add files via upload

---
+++ JavaSelenium/src/main/java/constants/BrowserConstants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class BrowserConstants {
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String IE = "ie";
+}
---
+++ JavaSelenium/src/test/java/BaseTest.java
@@ -0,0 +1,27 @@
+import constants.BrowserConstants;
+import functions.Base;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import utils.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    public WebDriver webDriver;
+
+    @BeforeClass
+    public void init(){
+        webDriver = WebDriverFactory.getInstance(BrowserConstants.CHROME);
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        webDriver.manage().window().maximize();
+        webDriver.get("http://thedemosite.co.uk/addauser.php");
+        Base.webDriver = webDriver;
+    }
+
+
+    @AfterSuite
+    public void tearDown(){
+        webDriver.quit();
+    }
+}
