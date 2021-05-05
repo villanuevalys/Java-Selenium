32d2303c5723b5b4cb70c17e288694fb43b41a0b
villanuevalys <83683692+villanuevalys@users.noreply.github.com>
2021-05-06 01:35:47 +0800

    Delete JavaSelenium directory

--- JavaSelenium/src/main/java/constants/BrowserConstants.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-public class BrowserConstants {
-    public static final String CHROME = "chrome";
-    public static final String FIREFOX = "firefox";
-    public static final String IE = "ie";
-}
